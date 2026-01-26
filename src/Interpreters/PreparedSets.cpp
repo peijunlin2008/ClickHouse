6c621d32e4fbfc9a0f5920f9cc060a5ad5565e2c
Nikita Taranov <nikita.taranov@clickhouse.com>
2026-01-26 18:34:28 +0100

    stash

--- src/Interpreters/PreparedSets.cpp
+++ src/Interpreters/PreparedSets.cpp
@@ -25,6 +25,8 @@
 #include <Common/Logger.h>
 #include <Common/logger_useful.h>
 
+#include <Common/StackTrace.h>
+
 namespace DB
 {
 namespace Setting
@@ -74,6 +76,7 @@ DataTypes FutureSetFromStorage::getTypes() const { return set->getElementsTypes(
 
 SetPtr FutureSetFromStorage::buildOrderedSetInplace(const ContextPtr &)
 {
+    // LOG_DEBUG(&Poco::Logger::get("debug"), "__PRETTY_FUNCTION__={}, __LINE__={}", __PRETTY_FUNCTION__, __LINE__);
     return set->hasExplicitSetElements() ? set : nullptr;
 }
 
@@ -127,6 +130,7 @@ Columns FutureSetFromTuple::getKeyColumns()
 
 SetPtr FutureSetFromTuple::buildOrderedSetInplace(const ContextPtr & context)
 {
+    // LOG_DEBUG(&Poco::Logger::get("debug"), "__PRETTY_FUNCTION__={}, __LINE__={}", __PRETTY_FUNCTION__, __LINE__);
     if (set->hasExplicitSetElements())
         return set;
 
@@ -195,6 +199,7 @@ void FutureSetFromSubquery::setQueryPlan(std::unique_ptr<QueryPlan> source_)
 
 void FutureSetFromSubquery::buildExternalTableFromInplaceSet(StoragePtr external_table_)
 {
+    // LOG_DEBUG(&Poco::Logger::get("debug"), "__PRETTY_FUNCTION__={}, __LINE__={}", __PRETTY_FUNCTION__, __LINE__);
     const auto & set = *set_and_key->set;
 
     LOG_TRACE(getLogger("FutureSetFromSubquery"), "Building external table from set of {} elements", set.getTotalRowCount());
@@ -260,6 +265,7 @@ FutureSet::Hash FutureSetFromSubquery::getHash() const { return hash; }
 
 std::unique_ptr<QueryPlan> FutureSetFromSubquery::build(const SizeLimits & network_transfer_limits, const PreparedSetsCachePtr & prepared_sets_cache)
 {
+    // LOG_DEBUG(&Poco::Logger::get("debug"), "__PRETTY_FUNCTION__={}, __LINE__={}", __PRETTY_FUNCTION__, __LINE__);
     if (set_and_key->set->isCreated())
         return nullptr;
 
@@ -280,6 +286,7 @@ std::unique_ptr<QueryPlan> FutureSetFromSubquery::build(const SizeLimits & netwo
 
 void FutureSetFromSubquery::buildSetInplace(const ContextPtr & context)
 {
+    // LOG_DEBUG(&Poco::Logger::get("debug"), "__PRETTY_FUNCTION__={}, __LINE__={}", __PRETTY_FUNCTION__, __LINE__);
     if (external_table_set)
         external_table_set->buildSetInplace(context);
 
@@ -302,6 +309,12 @@ void FutureSetFromSubquery::buildSetInplace(const ContextPtr & context)
 
 SetPtr FutureSetFromSubquery::buildOrderedSetInplace(const ContextPtr & context)
 {
+    // LOG_DEBUG(
+    //     &Poco::Logger::get("debug"),
+    //     "__PRETTY_FUNCTION__={}, __LINE__={}, stack={}",
+    //     __PRETTY_FUNCTION__,
+    //     __LINE__,
+    //     StackTrace().toString());
     if (!context->getSettingsRef()[Setting::use_index_for_in_with_subqueries])
         return nullptr;
 
