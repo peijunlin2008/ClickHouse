4c897df0cf0c82c63c4a2076fdec4ddda04957c1
Hechem Selmi <hechem.selmi@clickhouse.com>
2026-02-06 10:22:46 +0000

    add test.

---
+++ tests/queries/0_stateless/03826_right_outer_join_runtime_filters.sql
@@ -0,0 +1,73 @@
+SET enable_join_runtime_filters = 1;
+
+CREATE TABLE nation(n_nationkey Int32, n_name String) ENGINE MergeTree ORDER BY n_nationkey;
+CREATE TABLE customer(c_custkey Int32, c_nationkey Int32) ENGINE MergeTree ORDER BY c_custkey;
+
+INSERT INTO nation VALUES (5,'ETHIOPIA'),(6,'FRANCE'),(7,'GERMANY');
+INSERT INTO customer SELECT number, 5 - (number % 2) FROM numbers(500);
+
+SET enable_parallel_replicas=0;
+SET query_plan_join_swap_table=0;
+SET join_algorithm='hash';
+
+SELECT REGEXP_REPLACE(trimLeft(explain), '_runtime_filter_\\d+', '_runtime_filter_UNIQ_ID')
+FROM (
+    EXPLAIN actions=1
+    SELECT count()
+    FROM customer RIGHT OUTER JOIN nation
+    ON c_nationkey = n_nationkey
+)
+WHERE (explain ILIKE '%Filter column%') OR (explain LIKE '%BuildRuntimeFilter%') OR (explain LIKE '% Type:%') OR (explain LIKE '% Strictness:%');
+
+SELECT count()
+FROM customer RIGHT OUTER JOIN nation
+ON c_nationkey = n_nationkey
+SETTINGS enable_join_runtime_filters = 0;
+
+SELECT count()
+FROM customer RIGHT OUTER JOIN nation
+ON c_nationkey = n_nationkey
+SETTINGS enable_join_runtime_filters = 1;
+
+SELECT 'No runtime filter for LEFT OUTER';
+SELECT count()
+FROM (
+    EXPLAIN actions=1
+    SELECT count()
+    FROM nation LEFT OUTER JOIN customer
+    ON c_nationkey = n_nationkey
+)
+WHERE (explain ILIKE '%Filter column%') OR (explain LIKE '%BuildRuntimeFilter%');
+
+SELECT '1 element in filter';
+SELECT count()
+FROM customer RIGHT OUTER JOIN nation
+ON c_nationkey = n_nationkey
+WHERE n_name = 'FRANCE';
+
+SELECT '0 elements in filter';
+-- 'WAKANDA' is not present in `nations` table
+SELECT count()
+FROM customer RIGHT OUTER JOIN nation
+ON c_nationkey = n_nationkey
+WHERE n_name = 'WAKANDA';
+
+SELECT 'Again 1 element in filter';
+SELECT count()
+FROM customer RIGHT OUTER JOIN nation
+ON c_nationkey = n_nationkey
+WHERE n_name IN ('WAKANDA', 'GERMANY');
+
+SELECT '2 elements in filter';
+SELECT count()
+FROM customer RIGHT OUTER JOIN nation
+ON c_nationkey = n_nationkey
+WHERE n_name IN ('FRANCE', 'GERMANY');
+
+SELECT '2 elements in filter in bloom filter';
+SET join_runtime_filter_exact_values_limit = 1;
+
+SELECT count()
+FROM customer RIGHT OUTER JOIN nation
+ON c_nationkey = n_nationkey
+WHERE n_name IN ('FRANCE', 'GERMANY');
