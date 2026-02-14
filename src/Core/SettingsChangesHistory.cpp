cf4239c17952f5a31bd21cd1879e47fd50984bf5
Konstantin Vedernikov <vedernikovkostya@mail.ru>
2026-02-14 00:24:18 +0000

    fix

--- src/Core/SettingsChangesHistory.cpp
+++ src/Core/SettingsChangesHistory.cpp
@@ -51,8 +51,7 @@ const VersionToSettingsChangesMap & getSettingsChangesHistory()
             {"use_page_cache_for_object_storage", false, false, "New setting to use userspace page cache for object storage table functions"},
             {"use_statistics_cache", false, true, "Enable statistics cache"},
             {"ignore_format_null_for_explain", false, true, "FORMAT Null is now ignored for EXPLAIN queries by default"},
-            {"allow_experimental_insert_into_iceberg", false, true, "Insert into iceberg was moved to Beta"},
-            {"allow_insert_into_iceberg", false, true, "Insert into iceberg was moved to Beta. Added an alias for setting 'allow_experimental_insert_into_iceberg'."},
+            {"allow_insert_into_iceberg", false, true, "Insert into iceberg was moved to Beta"},
         });
         addSettingsChanges(settings_changes_history, "26.1",
         {
