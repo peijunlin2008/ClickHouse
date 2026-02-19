f0c5c607b24b9f701199d00595d15d8ece9ac754
Nikita Taranov <nikita.taranov@clickhouse.com>
2026-02-19 10:46:19 +0000

    fix test

--- tests/queries/0_stateless/00328_long_case_construction.sql
+++ tests/queries/0_stateless/00328_long_case_construction.sql
@@ -3,6 +3,9 @@
 -- This test contains > 1500 queries and apparently the overhead of re-interpretation with enabled parallel replicas is too high.
 SET automatic_parallel_replicas_mode=0;
 
+-- Too slow for targeted tests
+SET compile_aggregate_expressions=0, compile_expressions=0;
+
 /* Trivial case */
 
 SELECT CASE WHEN 1 THEN 2 WHEN 3 THEN  4 ELSE 5 END;
