91bc791fcb3685e532ef648d62fde8473e87af5a
Visakh Unnikrishnan <visakh@clusterdev.net>
2026-02-20 17:27:03 +0530

    add more tests

--- tests/queries/0_stateless/03919_startsWithUTF8_use_primary_key.sql
+++ tests/queries/0_stateless/03919_startsWithUTF8_use_primary_key.sql
@@ -1,11 +1,37 @@
 DROP TABLE IF EXISTS test_startsWithUTF8;
 CREATE TABLE test_startsWithUTF8 (a String) Engine = MergeTree() ORDER BY a;
 
-INSERT INTO test_startsWithUTF8 (a) values ('a'), ('abcd'), ('bbb'), (''), ('abc');
+INSERT INTO test_startsWithUTF8 (a) VALUES ('a'), ('abcd'), ('bbb'), (''), ('abc');
 
-SELECT count() from test_startsWithUTF8 where startsWithUTF8(a, 'a') settings force_primary_key=1;
-SELECT count() from test_startsWithUTF8 where startsWithUTF8(a, '🙂') settings force_primary_key=1; -- { serverError INDEX_NOT_USED }
-SELECT count() FROM test_startsWithUTF8 WHERE startsWithUTF8('a', a);
-SELECT count() FROM test_startsWithUTF8 WHERE startsWithUTF8('a', a) SETTINGS force_primary_key=1; -- { serverError INDEX_NOT_USED }
+SELECT count() from test_startsWithUTF8
+WHERE startsWithUTF8(a, 'a')
+SETTINGS force_primary_key=1;
+
+SELECT count() from test_startsWithUTF8
+WHERE startsWithUTF8(a, '🙂')
+SETTINGS force_primary_key=1; -- { serverError INDEX_NOT_USED }
+
+SELECT count() FROM test_startsWithUTF8
+WHERE startsWithUTF8('a', a)
+SETTINGS force_primary_key=1; -- { serverError INDEX_NOT_USED }
+
+SELECT count() FROM test_startsWithUTF8
+WHERE startsWithUTF8('a', a);
+
+SELECT count() FROM test_startsWithUTF8
+WHERE startsWithUTF8('a', a)
+SETTINGS force_primary_key = 1; -- { serverError INDEX_NOT_USED }
+
+SELECT count() FROM test_startsWithUTF8
+WHERE startsWithUTF8(a, '')
+SETTINGS force_primary_key = 1; -- { serverError INDEX_NOT_USED }
+
+SELECT count() FROM test_startsWithUTF8
+WHERE startsWithUTF8(a, substring(a, 1, 1))
+SETTINGS force_primary_key = 1; -- { serverError INDEX_NOT_USED }
+
+SELECT count() FROM test_startsWithUTF8
+WHERE startsWithUTF8(a, concat('a', ''))
+SETTINGS force_primary_key = 1;
 
 DROP TABLE test_startsWithUTF8;
